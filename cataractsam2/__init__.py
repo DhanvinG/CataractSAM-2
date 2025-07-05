c900a69e3cde00484b6089d12e763301b341c23b
Dhanvinkumar Ganeshkumar <117781489+DhanvinG@users.noreply.github.com>
2025-07-05 18:54:38 -0400

    Clarify Predictor docstring

--- cataractsam2/__init__.py
+++ cataractsam2/__init__.py
@@ -1,8 +1,9 @@
-from .ui_widget import Object, Reset, Visualize, Propagate, video_segments
+from .ui_widget import Object, Reset, Visualize, Propagate, setup, video_segments
 from .masks     import Masks
 from .predictor import Predictor
 
 __all__ = [
+    "setup",
     "Object", "Reset", "Visualize", "Propagate",
     "Masks",
     "Predictor",
