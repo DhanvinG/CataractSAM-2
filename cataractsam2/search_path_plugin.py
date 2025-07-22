b462ee40091ba8967468fd828b72f832c6d85f8c
Dhanvinkumar Ganeshkumar <117781489+DhanvinG@users.noreply.github.com>
2025-07-22 13:29:40 -0400

    Update search_path_plugin.py

--- cataractsam2/search_path_plugin.py
+++ cataractsam2/search_path_plugin.py
@@ -2,10 +2,8 @@ from hydra.core.plugins import SearchPathPlugin
 from hydra.core.utils import SearchPath, SearchPathItem
 
 class CataractSAM2SearchPathPlugin(SearchPathPlugin):
-    """Hydra plugin that registers the package's default configuration."""
 
     def manipulate_search_path(self, search_path: SearchPath) -> None:
-        """Append CataractSAM2's config directory to Hydra's search paths."""
         search_path.append(
             SearchPathItem(
                 provider="cataractsam2_cfg",
